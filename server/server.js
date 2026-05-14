57d7cd9a2421000fdec88a97bd1ac39abb40f859
Puskar10 <puskarshaw346@gmail.com>
2026-05-14 15:45:34 +0530

    fix cors origin

--- server/server.js
+++ server/server.js
@@ -10,26 +10,29 @@ import conversationRoutes from "./routes/conversationRoutes.js";
 
 const app = express();
 
-//console.log(process.env.GROQ_API_KEY);
-// console.log(process.env.MONGO_URI);
-
 /* -------- DATABASE -------- */
 
 connectDB();
 
 /* -------- MIDDLEWARE -------- */
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://jarvis-ai-chatbot-delta.vercel.app/"],
-    methods: ["GET", "POST"],
-    credentials: true,
-  }),
-);
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://jarvis-ai-chatbot-delta.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 
 /* -------- ROUTES -------- */
+
 app.use("/auth", authRoutes);
 app.use("/chat", chatRoutes);
 app.use("/conversations", conversationRoutes);
